aacfe0c865b846f0ddb047da30a4ebd495bea84d
Dan Beil <dan@alleyinteractive.com>
2015-01-23 11:02:31 -0600

    switching to use FM_Dash_Widget class

---
+++ test-widget.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * undocumented class
+ *
+ * @package default
+ * @author
+ **/
+class Test_Widget {
+
+	public function __construct() {
+		add_action( 'init', array( $this, 'test_widget' ) );
+	}
+
+	/**
+	 * This is what would be in the theme files
+	 * Current limitation: is that we MUST use a group to avoid saving the entire $_POST object
+	 * Current limitation: does not support repeating fields due to the fact that you cannot set default values ( I think )
+	 */
+	public function test_widget() {
+
+		$name = 'meta_fields'; // uber important, used for the name of the group and the key for saving in wp_options
+
+		$values = get_option( $name );
+
+		$months = array( 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' );
+
+		$fm = new Fieldmanager_Group( array(
+			'name'     => $name,
+			'label' => 'Test widget label',
+			'children' => array(
+				'text'         => new Fieldmanager_Textfield( 'Text Field', array(
+					'default_value' => $values['text'],
+				) ),
+				'autocomplete' => new Fieldmanager_Autocomplete( 'Autocomplete', array(
+					'datasource' => new Fieldmanager_Datasource_Post(),
+					'default_value' => $values['autocomplete'],
+				) ),
+				'local_data'   => new Fieldmanager_Autocomplete( 'Autocomplete without ajax', array(
+					'datasource' => new Fieldmanager_Datasource( array( 'options' => $months ) ),
+					'default_value' => $values['local_data'],
+				) ),
+				'textarea'     => new Fieldmanager_TextArea( 'TextArea', array(
+					'default_value' => $values['textarea'],
+				) ),
+				'media'        => new Fieldmanager_Media( 'Media File', array(
+					'default_value' => $values['media'],
+				) ),
+				'checkbox'     => new Fieldmanager_Checkbox( 'Checkbox', array(
+					'default_value' => $values['checkbox'],
+				) ),
+				'radios'       => new Fieldmanager_Radios( 'Radio Buttons', array(
+					'options' => array( 'One', 'Two', 'Three' ),
+					'default_value' => $values['radios'],
+				) ),
+				'select'       => new Fieldmanager_Select( 'Select Dropdown', array(
+					'options' => array( 'One', 'Two', 'Three' ),
+					'default_value' => $values['select'],
+				) ),
+				'richtextarea' => new Fieldmanager_RichTextArea( 'Rich Text Area', array(
+					'default_value' => $values['richtextarea'],
+				) ),
+			)
+		) );
+
+		$text_widget = new FM_Dash_Widget( $fm );
+
+	} // end test_widget
+
+} // END class
+new Test_Widget();
--- zzz-fm-dash-widget.php
+++ zzz-fm-dash-widget.php
@@ -36,30 +36,20 @@ if ( defined( 'FM_VERSION' ) && ! class_exists( 'FM_Dash_Widget' ) ) {
 		 * @param [type] $widget_id [description]
 		 * @param [type] $label     [description]
 		 */
-		public function __construct( $widget_id, $label ) {
-			$this->widget_id = $widget_id;
-			$this->label = $label;
+		public function __construct( $field ) {
+			$this->field = $field;
+			$this->widget_id = $field->name;
+			$this->label = $field->label;
 			if ( ! empty( $this->widget_id ) ) {
 				add_action( 'wp_dashboard_setup', array( $this, 'add_dashboard_widget' ) );
 			}
 		}
 
 		/**
-		 * [return_html description]
-		 * @param  [type] $type  [description]
-		 * @param  [type] $name  [description]
-		 * @param  [type] $class [description]
-		 * @return [type]        [description]
-		 */
-		public function return_html( $type, $name, $class = null ) {
-			$widget = get_option( $this->widget_id );
-			echo sprintf( '<input type="%s" name="%s[%s]" value="%s" />', esc_attr( $type ), esc_attr( $this->widget_id ), esc_attr( $name ), esc_attr( $widget[ $name ] ) );
-		}
-
-		/**
 		 * [add_dashboard_widget description]
 		 */
 		public function add_dashboard_widget() {
+
 			wp_add_dashboard_widget(
 				$this->widget_id,
 				$this->label,
@@ -75,26 +65,24 @@ if ( defined( 'FM_VERSION' ) && ! class_exists( 'FM_Dash_Widget' ) ) {
 		public function render_dashboard_widget() {
 			// displaying the print_r of get_options for this widget
 			// In a production envrio this would display options in a pretty way
-			echo '<pre>';
+			echo 'get option<pre>';
 			print_r( get_option( $this->widget_id ) );
 			echo '</pre>';
+
 		}
 
 		/**
-		 * [configure_dashboard_widget description]
+		 * The config screen for the widget
 		 * @return [type] [description]
 		 */
 		public function configure_dashboard_widget() {
 
-			$fm = new Fieldmanager_Textfield( 'test fm', array(
-				'name' => 'test_fm_save',
-			) );
-			$this->return_html( $fm->field_class, $fm->name ); // this needs to use FM core rather than this function
+			echo $this->field->element_markup( $this->field->name );
 
 			echo '<input type="hidden" name="' . $this->widget_id . '_fm_save_check" value="true" />';
 			echo '<br /><hr />';
 
-			// Creating array for saving so we dont save all $_POST data here
+			// Using the $field->name to distinguish what should be saved
 			if ( isset( $_POST[ $this->widget_id . '_fm_save_check' ] ) ) {
 				update_option( $this->widget_id, $_POST[ $this->widget_id ] );
 			}
@@ -105,7 +93,5 @@ if ( defined( 'FM_VERSION' ) && ! class_exists( 'FM_Dash_Widget' ) ) {
 
 } // end FM check
 
-
-new FM_Dash_Widget( 'test_id_1', __( 'Label', 'textdomain' ) );
-
-new FM_Dash_Widget( 'test_widget_2', __( 'second label', 'textdomain' ) );
+// including the files that represents theme level code, i.e. what we just build all ^ that for
+include( 'test-widget.php' );
